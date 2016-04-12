c0ebe08b0d7c812fcaa5fa82ec4f8b739db48377
RoBareiro <rsb.bareiro@live.com.ar>
2016-04-12 12:03:01 -0300

    Test del Triangulo
    Codificiacion del Test del Triangulo

--- test/ar/edu/unlam/tallerweb/triangulo/TestTriangulo.java
+++ test/ar/edu/unlam/tallerweb/triangulo/TestTriangulo.java
@@ -1,14 +1,22 @@
 package ar.edu.unlam.tallerweb.triangulo;
 
-import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 public class TestTriangulo {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testQueCalculaElArea(){
+		Triangulo trianguloPrueba = new Triangulo(2.0, 5.0);
+		Double resultado = trianguloPrueba.calcularAreaTriangulo();
+		Assert.assertEquals(1.73, resultado, 0.01);
 	}
 
+	@Test
+	public void testQueCalculaElPerimetro(){
+		Triangulo trianguloPrueba = new Triangulo(5.0, 4.0);
+		Double resultado = trianguloPrueba.calcularPerimetroTriangulo();
+		Assert.assertEquals(15.0, resultado, 0.01);
+	}
 }
